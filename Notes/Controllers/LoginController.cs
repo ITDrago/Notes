84697df2523ea65abee9b021aa93a313d38aa739
Мартин Симонян <simonyan.martin@rambler.ru>
2023-07-12 21:05:37 +0300

    The introduction of registration and the process itself was made, the design was changed

---
+++ Notes/Controllers/LoginController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Notes.Data;
+using Notes.Models;
+using Notes.Views.Login;
+
+namespace Notes.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+        public LoginController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
+        { 
+            _context = context;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return Redirect("/Home/Index");
+            }
+            var response = new LoginViewModel();
+            return View(response);
+        }
+    }
+}
