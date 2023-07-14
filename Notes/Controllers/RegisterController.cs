de8d34560a0fcfae378beb4536be41aac3c0ee10
Мартин Симонян <simonyan.martin@rambler.ru>
2023-07-14 13:18:42 +0300

    The registration and authorization pages have been fixed, the design has been changed and the authorization and authentication system has been tested

--- Notes/Controllers/RegisterController.cs
+++ Notes/Controllers/RegisterController.cs
@@ -1,12 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Notes.Data;
+using Notes.Models;
+using Notes.Views.Register;
 
 namespace Notes.Controllers
 {
     public class RegisterController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+        public RegisterController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
         {
-            return View();
+            _context = context;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            var response = new RegisterViewModel();
+            return View(response);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        {
+            if (!ModelState.IsValid) return View(registerViewModel);
+
+            var user = await _userManager.FindByEmailAsync(registerViewModel.EmailAddress);
+
+            if (user != null)
+            {
+                TempData["Error"] = "This email addresses is already in use";
+                return View(registerViewModel);
+            }
+            var newUser = new AppUser()
+            {
+                Email = registerViewModel.EmailAddress,
+                UserName = registerViewModel.EmailAddress
+            };
+
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerViewModel.Password);
+            if (newUserResponse.Succeeded)
+                await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+            return RedirectToAction("Login", "Login");
         }
     }
 }
