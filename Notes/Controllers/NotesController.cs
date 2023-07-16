92764c17345ccb98293bf993f2eac3b84324adfe
Мартин Симонян <simonyan.martin@rambler.ru>
2023-07-16 17:08:18 +0300

    The interface has been improved, saving notes to the database has been implemented, and the pages themselves have been created for these controllers

---
+++ Notes/Controllers/NotesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Notes.Data;
+using Notes.Models;
+
+namespace Notes.Controllers
+{
+    public class NotesController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+        public NotesController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager, ApplicationDbContext context)
+        {
+            _context = context;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult AllNotes()
+        {
+            IEnumerable<Note> notes = _context.Notes.ToList();
+            return View(notes);
+           
+        }
+        [HttpPost]
+        public IActionResult AddNotes(string comment, string name)
+        {
+            if(comment!= null && name != null)
+            {
+                var note = new Note() { Name = name, Content = comment};
+                _context.Notes.Add(note);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+    }
+}
